6e47a7d7f28e11a8919686702d4327371025874e
Abdias Saint Louis <abiwriter.sp@gmail.com>
2022-01-24 01:07:48 -0300

    feat: Add logout button

---
+++ AbiBank/AbiBank/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  AbiBank
+//
+//  Created by Abdias Saint-Louis on 24/01/22.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
